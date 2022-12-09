f18a6a0cdde7067497ecbc111afd21f19d82eaa3
Miguel santos <miguel.santos6700@gmail.com>
2022-12-09 15:20:51 -0300

    Added route in Post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,6 @@
 import styles from '../styles/postComponent.module.css'
 import { User } from 'phosphor-react'
+import Link from 'next/link'
 
 export default function Post(props) {
 
@@ -7,17 +8,16 @@ export default function Post(props) {
 
   return (
     <div className={styles.post}>
-      <div className={styles.postHeader}>
-        <h3> { title } </h3>
-        <User className={styles.userIcon}size={40} />
-      </div>
-
-      <hr/> 
-
-        <div className={styles.description}>
-          <h4> {props.description } </h4>
+        <Link className={styles.link} href={`/post/${props.title}`}>
+        <div className={styles.postHeader}>
+          <h3> { title } </h3>
+          <User className={styles.userIcon}size={40} />
         </div>
-
-  </div>
+        <hr/>
+          <div className={styles.description}>
+            <h4> {props.description } </h4>
+          </div>
+         </Link>
+        </div>
   )
 } 
